df2fdbb2badc9a9a7f9232135f898784906ff307
ApMatheus <matheusaparecido101@gmal.com>
2021-11-22 09:13:51 -0300

    1 teste

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Modal
 function abreModal() {
     let containerModal = document.querySelector('.container-modal');
     let modal = document.querySelector('.modal');
@@ -26,3 +27,25 @@ receitas.forEach(function (e) {
 window.addEventListener("scroll", function(){
     fechaModal();
 })
+
+//Menu mobile
+const btnNvaigation = () => {
+    const navList = document.getElementById("nav-list");
+    const item1 = document.querySelector(".item1");
+    const item2 = document.querySelector(".item2");
+    const item3 = document.querySelector(".item3");
+    if (navList.className == 'nav-list active') {
+        navList.classList.remove("active");
+        item1.classList.remove("active");
+        item2.classList.remove("active");
+        item3.classList.remove("active");
+    } else {
+        navList.classList.add("active");
+        item1.classList.add("active");
+        item2.classList.add("active");
+        item3.classList.add("active");
+    }
+}
+
+const navigationMobile = document.getElementById("mobile-navigation");
+navigationMobile.addEventListener("click", btnNvaigation);
